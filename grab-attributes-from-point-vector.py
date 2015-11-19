1f99deb3de27e5f2d57f9615959fd4a9fb460d63
Colin Stief <colin.stief@gmail.com>
2015-11-18 19:43:05 -0500

    Changes to scripts

--- grab-attributes-from-point-vector.py
+++ grab-attributes-from-point-vector.py
@@ -3,36 +3,43 @@ __author__ = 'Colin'
 import arcpy
 
 #primary_layer = arcpy.GetParameterAsText(0)
-#attribute_layer = arcpy.GetParameterAsText(1)
-#fields = arcpy.GetParameterAsText(2)
+#unique_field = arcpy.GetParameterAsText(1)
+#calculate_field = arcpy.GetParameterAsText(2)
+
+#attribute_layer = arcpy.GetParameterAsText(3)
+#grab_field = arcpy.GetParameterAsText(4)
+
 primary_layer_raw = "\\\\Ccsvr01\\d\\GIS\\PG_County\\Fields.gdb\\Parcels_Gaps"
 primary_layer = arcpy.MakeFeatureLayer_management(primary_layer_raw, "primary_layer")
-attribute_layer = "\\\\Ccsvr01\\d\\GIS\\PG_County\\CleanDatasets\\additional_data\\bmps.mdb\\bmps\\bmps"
+attribute_layer = "\\\\Ccsvr01\\d\\GIS\\PG_County\\CleanDatasets\\additional_data\\GISDEV_WSHED_12.shp"
 
-fields = ["TARGET_FID", "STRU_NO"]
+join_field = "TARGET_FID"
+fields = [join_field, "DNR12DIG"]
 
 values = {}
 
-#memory_join = "\\\\Ccsvr01\\d\\GIS\\PG_County\\Fields.gdb\\joined_features
 memory_join = "in_memory\\joined_features"
 arcpy.SpatialJoin_analysis(primary_layer, attribute_layer, memory_join, "JOIN_ONE_TO_MANY", "KEEP_COMMON", "", "INTERSECT", "","")
+
 rows = arcpy.da.SearchCursor(memory_join, fields)
 for row in rows:
     target_fid = row[0]
-    bmp_id = row[1]
+    grab_attribute = row[1]
 
     if target_fid in values:
-        bmp_list = values[target_fid]
-        bmp_list.append(bmp_id)
-        values[target_fid] = bmp_list
+        attribute_list = values[target_fid]
+        attribute_list.append(grab_attribute)
+        values[target_fid] = attribute_list
 
     else:
-        values[target_fid] = [bmp_id]
+        values[target_fid] = [grab_attribute]
 
 for item in values:
     sql = "OBJECTID = " + str(item)
+    #arcpy.AddMessage(sql)
     arcpy.SelectLayerByAttribute_management(primary_layer, "NEW_SELECTION", sql)
     list_string = "'" + ", ".join(values[item]) + "'"
-    arcpy.CalculateField_management(primary_layer, "BMP_ID", list_string, "Python")
+    #arcpy.AddMessage(list_string)
+    arcpy.CalculateField_management(primary_layer, "MD_Wat", list_string, "Python")
 
     arcpy.AddMessage("Finished with " + str(item))
---
+++ grab-attributes-from-polygon-vector.py
@@ -0,0 +1,41 @@
+__author__ = 'Colin'
+
+import arcpy
+
+arcpy.env.overwriteOutput = True
+
+parcel_file = "\\\\Ccsvr01\\d\\GIS\\PG_County\\Fields.gdb\\Parcels_Gaps"
+parcel_layer = arcpy.MakeFeatureLayer_management(parcel_file, "parcel_layer")
+
+chop_file = "\\\\Ccsvr01\\d\\GIS\\PG_County\\CleanDatasets\\additional_data\\GISDEV_WSHED_12.shp"
+chop_layer = arcpy.MakeFeatureLayer_management(chop_file, "chop_layer")
+
+fields = ["DNR12DIG"]
+
+total_count = 0
+
+rows = arcpy.da.SearchCursor(chop_file, fields)
+for row in rows:
+
+    huc = row[0]
+    huc_stripped = huc.lstrip("0")
+    sql = "DNR12DIG = " + "'" + huc + "'"
+    print sql
+
+    arcpy.SelectLayerByAttribute_management(chop_layer,"NEW_SELECTION", sql)
+
+    current_chop_layer = arcpy.MakeFeatureLayer_management(chop_layer, "current_chop_layer")
+    arcpy.SelectLayerByLocation_management(parcel_layer, "INTERSECT", current_chop_layer)
+
+    codeblock = """def grabValue(existingValue):
+        if existingValue != "":
+            newValue = existingValue + ", " + %s
+            return newValue
+
+        else:
+            newValue = %s
+            return newValue
+    """ % (huc_stripped, huc_stripped)
+
+    expression = "grabValue(str(!MD_Wat!))"
+    arcpy.CalculateField_management(parcel_layer, "MD_Wat", expression, "Python", codeblock)
