78b461bbd9745aac49f8f5ac4cfdadf052a80d38
Colin Stief <colin.stief@gmail.com>
2015-11-20 13:13:05 -0500

    Updated for codeblock to actually work

--- grab-attributes-from-polygon-vector.py
+++ grab-attributes-from-polygon-vector.py
@@ -18,18 +18,17 @@ rows = arcpy.da.SearchCursor(chop_file, fields)
 for row in rows:
 
     huc = row[0]
-    huc_stripped = huc.lstrip("0")
+    huc_stripped_unicode = huc.lstrip("0")
+    huc_stripped_string = huc_stripped_unicode.encode("utf-8")
+    huc_stripped = "'" + huc_stripped_string + "'"
     sql = "DNR12DIG = " + "'" + huc + "'"
-    print sql
 
     arcpy.SelectLayerByAttribute_management(chop_layer,"NEW_SELECTION", sql)
 
     current_chop_layer = arcpy.MakeFeatureLayer_management(chop_layer, "current_chop_layer")
-    arcpy.SelectLayerByLocation_management(parcel_layer, "INTERSECT", current_chop_layer)
+    arcpy.SelectLayerByLocation_management(parcel_layer, "INTERSECT", current_chop_layer, "", "NEW_SELECTION")
 
     codeblock = """def grabValue(existingValue):
-
-        arcpy.AddMessage(existingValue)
 
         if existingValue != "":
             newValue = existingValue + ", " + %s
