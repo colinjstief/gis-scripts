4ada5cc7d35d89a582589112e4282c04d69ba007
Colin Stief <colin.stief@gmail.com>
2015-11-27 15:40:55 -0500

    Altered script

--- grab-attributes-from-polygon-vector.py
+++ grab-attributes-from-polygon-vector.py
@@ -7,21 +7,31 @@ arcpy.env.overwriteOutput = True
 parcel_file = "\\\\Ccsvr01\\d\\GIS\\PG_County\\Fields.gdb\\Parcels_Gaps"
 parcel_layer = arcpy.MakeFeatureLayer_management(parcel_file, "parcel_layer")
 
-chop_file = "\\\\Ccsvr01\\d\\GIS\\PG_County\\CleanDatasets\\additional_data\\GISDEV_WSHED_12.shp"
+# EDIT 1
+chop_file = "\\\\Ccsvr01\\d\\GIS\\PG_County\\CleanDatasets\\storm_drain_inventory\\SDI.mdb\\Pipes_Dissolve_MP"
 chop_layer = arcpy.MakeFeatureLayer_management(chop_file, "chop_layer")
 
-fields = ["DNR12DIG"]
+# EDIT 2
+field = "SD_NUMBER"
 
-total_count = 0
+# EDIT 3
+fields = ["SD_NUMBER"]
 
 rows = arcpy.da.SearchCursor(chop_file, fields)
 for row in rows:
+#codes = ['01','02','03','04','05','06','07','08','09','10','11','13','14']
 
-    huc = row[0]
-    huc_stripped_unicode = huc.lstrip("0")
-    huc_stripped_string = huc_stripped_unicode.encode("utf-8")
-    huc_stripped = "'" + huc_stripped_string + "'"
-    sql = "DNR12DIG = " + "'" + huc + "'"
+#for code in codes:
+    #value_raw = code
+    value_raw = row[0]
+    #value = value_raw.lstrip("0")
+    sql = field + " = " + "'" + value_raw + "'"
+
+    #value_stripped_unicode = value.lstrip("0")
+    #value = value_raw.encode("utf-8")
+    #value_stripped = "'" + value_stripped_string + "'"
+
+    arcpy.AddMessage(sql)
 
     arcpy.SelectLayerByAttribute_management(chop_layer,"NEW_SELECTION", sql)
 
@@ -29,15 +39,16 @@ for row in rows:
     arcpy.SelectLayerByLocation_management(parcel_layer, "INTERSECT", current_chop_layer, "", "NEW_SELECTION")
 
     codeblock = """def grabValue(existingValue):
-
-        if existingValue != "":
-            newValue = existingValue + ", " + %s
+        if existingValue != "NA":
+            newValue = existingValue + ", " + "%s"
             return newValue
-
         else:
-            newValue = %s
+            newValue = "%s"
             return newValue
-    """ % (huc_stripped, huc_stripped)
+    """ % (value_raw, value_raw)
 
-    expression = "grabValue(str(!MD_Wat!))"
-    arcpy.CalculateField_management(parcel_layer, "MD_Wat", expression, "Python", codeblock)
+    # EDIT 4
+    expression = "grabValue(!Pipe_ID!)"
+
+    # EDIT 5
+    arcpy.CalculateField_management(parcel_layer, "Pipe_ID", expression, "Python", codeblock)
